5416690d057facae8ca15fdc4721e0c86f3d26ec
prado-rafa <rps4@cin.ufpe.br>
2021-03-14 19:00:48 -0300

    feat(sellings-control): add date section

--- sellings-control.js
+++ sellings-control.js
@@ -1,79 +1,106 @@
-const _ = require('lodash');
-const prompt = require('prompt-sync')({sigint: true});
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const _ = require("lodash");
+const prompt = require("prompt-sync")({ sigint: true });
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 let sellersAmounts = new Map();
 
 let sellings = [];
 
 const readFiles = () => {
-  sellersAmounts = new Map(JSON.parse(fs.readFileSync('./amounts.json', 'utf8')));
-  sellings = JSON.parse(fs.readFileSync('./sellings.json', 'utf8'))
-}
+  sellersAmounts = new Map(
+    JSON.parse(fs.readFileSync("./amounts.json", "utf8"))
+  );
+  sellings = JSON.parse(fs.readFileSync("./sellings.json", "utf8"));
+};
 
-const setSellerName =  sellToBeRegistered  => {
-  const sellerName = prompt('Seller name: ');
+const setSellerName = sellToBeRegistered => {
+  const sellerName = prompt("Seller name: ");
 
-  if(!sellersAmounts.has(sellerName)){
-    console.log("Seller not registered :'(")
-    setSellerName(sellToBeRegistered)
+  if (!sellersAmounts.has(sellerName)) {
+    console.log("Seller not registered :'(");
+    setSellerName(sellToBeRegistered);
   } else {
     sellToBeRegistered.sellerName = sellerName;
   }
-}
+};
 
-const setCustomerName =  sellToBeRegistered  => {
-  const customerName = prompt('Customer name: ');
+const setCustomerName = sellToBeRegistered => {
+  const customerName = prompt("Customer name: ");
 
-  if(_.isEmpty(customerName)){
-    console.log("Invalid name :'(")
-    setCustomerName(sellToBeRegistered)
+  if (_.isEmpty(customerName)) {
+    console.log("Invalid name :'(");
+    setCustomerName(sellToBeRegistered);
   } else {
     sellToBeRegistered.customerName = customerName;
   }
-}
+};
 
-const setItemName =  sellToBeRegistered  => {
-  const itemName = prompt('Item name: ');
+const setDate = sellToBeRegistered => {
+  const date = prompt("Selling date (dd/mm/yyyy): ");
+  const dateSplitted = date.split("/");
 
-  if(_.isEmpty(itemName)){
-    console.log("Invalid name :'(")
-    setItemName(sellToBeRegistered)
-  } else{
+  const validDate =
+    dateSplitted.length === 3 &&
+    _.every(
+      dateSplitted,
+      fragment => !_.isNaN(_.toNumber(fragment)) && _.toNumber(fragment) > 0
+    );
+
+  if (!validDate) {
+    console.log("Invalid date :'(");
+    setDate(sellToBeRegistered);
+  } else {
+    sellToBeRegistered.date = date;
+  }
+};
+
+const setItemName = sellToBeRegistered => {
+  const itemName = prompt("Item name: ");
+
+  if (_.isEmpty(itemName)) {
+    console.log("Invalid name :'(");
+    setItemName(sellToBeRegistered);
+  } else {
     sellToBeRegistered.itemName = itemName;
   }
-}
+};
 
-const setValue =  sellToBeRegistered  => {
-  const value = _.toNumber(prompt('Value: '));
+const setValue = sellToBeRegistered => {
+  const value = _.toNumber(prompt("Value: "));
 
-  if(!_.isNumber(value)){
-    console.log("Invalid value :'(")
-    setValue(sellToBeRegistered)
-  } else{
+  if (_.isNaN(value) || value <= 0) {
+    console.log("Invalid value :'(");
+    setValue(sellToBeRegistered);
+  } else {
     sellToBeRegistered.value = value;
   }
-}
+};
 
-function registerSelling(sellToBeRegistered){
+function registerSelling(sellToBeRegistered) {
   sellings.push(sellToBeRegistered);
 
-  fs.writeFileSync('./sellings.json', JSON.stringify(sellings));
+  fs.writeFileSync("./sellings.json", JSON.stringify(sellings));
 
   sellersAmounts.set(
     sellToBeRegistered.sellerName,
     sellersAmounts.get(sellToBeRegistered.sellerName) + sellToBeRegistered.value
   );
 
-  fs.writeFileSync('./amounts.json', JSON.stringify([...sellersAmounts]));
+  fs.writeFileSync("./amounts.json", JSON.stringify([...sellersAmounts]));
 }
 
-function printList(){
-  const sortedSellings =  _.sortBy(sellings, ({sellerName}) => -1 * sellersAmounts.get(sellerName));
-  const transformed = sortedSellings.reduce((aux, {uuid, ...x}) => { aux[uuid] = x; return aux}, {})
+function printList() {
+  const sortedSellings = _.sortBy(
+    sellings,
+    ({ sellerName }) => -1 * sellersAmounts.get(sellerName)
+  );
+  const transformed = sortedSellings.reduce((aux, { uuid, ...x }) => {
+    aux[uuid] = x;
+    return aux;
+  }, {});
 
-  console.log("List sorted by sellers with the highest to lowest amount sold.")
+  console.log("List sorted by sellers with the highest to lowest amount sold.");
   console.table(transformed);
 }
 
@@ -85,10 +112,11 @@ function startRegister() {
     date: null,
     itemName: null,
     value: null
-  }
+  };
 
-  setSellerName(sellToBeRegistered)
+  setSellerName(sellToBeRegistered);
   setCustomerName(sellToBeRegistered);
+  setDate(sellToBeRegistered);
   setItemName(sellToBeRegistered);
   setValue(sellToBeRegistered);
 
@@ -104,9 +132,8 @@ console.log("Complete! Let's register a sell. o/");
 let another = true;
 do {
   startRegister();
-  const answer = prompt("Register other? Write 'no' if not.");
-  another = answer !== 'no';
-
-} while(another);
+  const answer = prompt("Register other? Write 'no' if not.\n");
+  another = answer !== "no";
+} while (another);
 
 console.log("Bye!");
